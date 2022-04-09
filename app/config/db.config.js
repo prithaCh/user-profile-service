8510f91f6aac20625f320a7c73ff7ef031eb8e92
Pritha Chattopadhyay <Pritha.Chattopadhyay@gds.ey.com>
2022-04-09 11:18:45 +0530

    updated db connection string to common db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url: "mongodb://localhost:27017/events_management_db"
+	url: "mongodb+srv://assignment:assignment123@cluster0.4lw6a.mongodb.net/events_management_db?retryWrites=true&w=majority"
 };
